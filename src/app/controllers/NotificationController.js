39d38552c3668e150fc5628bc7c6eb5457ac93e8
Allan Amaral <allanmaral@hotmail.com>
2019-10-24 13:36:50 -0300

    feat: Added read notification route
    Added PUT notification at /notification/:id to update the
    notification an mark as read.

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -23,6 +23,16 @@ class NotificationController {
 
     return res.json(notifications);
   }
+
+  static async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true },
+    );
+
+    return res.json(notification);
+  }
 }
 
 export default NotificationController;
