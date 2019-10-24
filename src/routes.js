ce662140655050156679241a35380cfb8c7a1b84
Allan Amaral <allanmaral@hotmail.com>
2019-10-24 00:24:24 -0300

    feat: Added Schedule index route
    Added the listing of schedule for a provider at the route
    GET /schedules

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
@@ -21,10 +22,12 @@ routes.use(authMiddleware);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
+routes.post('/files', upload.single('file'), FileController.store);
+
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
-routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
